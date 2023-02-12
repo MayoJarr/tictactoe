ab1559f32cd4ed439603fc04eadbf4d8bd15bc21
MayoJarr <jakubmajecki2321@gmail.com>
2023-02-12 15:41:18 +0100

    n

--- script.js
+++ script.js
@@ -14,11 +14,30 @@ const Player = (sign) => {
   return { getPlayerSign };
 };
 
+const Npc = (() => {
+  const ar = GameBoard.array;
+  const newMove = () => Math.round(Math.random() * 9);
+
+  const makeMove = () => {
+    const move = newMove();
+    if (ar[move] === 'x' || ar[move] === 'o') makeMove();
+    if (ar[move] === '') {
+      addMove(move + 1);
+    }
+  };
+  const addMove = (move) => {
+    Game.addMark(move, 'o');
+  };
+  return { makeMove };
+})();
 const DisplayController = (() => {
   const items = document.querySelectorAll('.item');
   const menu = document.querySelector('.menu');
   const resetB = document.querySelector('.resetB');
   const menuText = document.querySelector('.menuText');
+  const twoP = document.querySelector('.twoP');
+  const oneP = document.querySelector('.oneP');
+  const greeting = document.querySelector('.greeting');
   const render = () => {
     items.forEach((item, index) => {
       item.textContent = GameBoard.array[index];
@@ -31,10 +50,13 @@ const DisplayController = (() => {
   };
   const showMenu = (winner) => {
     menuText.textContent = winner;
-    menu.style.cssText = 'top: 15px; opacity: 1; transform(scale(1.2));';
+    menu.style.cssText = 'top: 15px; opacity: 1;';
   };
   const hideMenu = () => {
-    menu.style.cssText = 'top: -300px; opacity: 0; transform(scale(1));';
+    menu.style.cssText = 'top: -300px; opacity: 0;';
+  };
+  const hideGreeting = () => {
+    greeting.style.cssText = 'opacity: 0; transform: scale(.2);';
   };
   items.forEach((element) => {
     element.addEventListener('click', () => {
@@ -47,9 +69,18 @@ const DisplayController = (() => {
     resetItems();
     Game.resetRound();
   });
+  oneP.addEventListener('click', () => {
+    Game.gameTypee('one');
+    hideGreeting();
+  });
+  twoP.addEventListener('click', () => {
+    Game.gameTypee('two');
+    hideGreeting();
+  });
   return { render, showMenu };
 })();
 const Game = (() => {
+  let gameType = '';
   const playerX = Player('x');
   const playerO = Player('o');
   let round = 0;
@@ -58,6 +89,9 @@ const Game = (() => {
     ar.splice(place - 1, 1);
     ar.splice(place - 1, 0, sign);
     DisplayController.render();
+  };
+  const gameTypee = (type) => {
+    gameType = type;
   };
   const checkIfWin = (s) => {
     const ar = GameBoard.array;
@@ -87,11 +121,22 @@ const Game = (() => {
     if (ar[place - 1] === 'x' || ar[place - 1] === 'o') return;
     if (ar[place - 1] === '') {
       round += 1;
-      if (round % 2 === 1) addMark(place, playerX.getPlayerSign());
-      else if (round % 2 === 0) addMark(place, playerO.getPlayerSign());
+      if (round % 2 === 1) {
+        addMark(place, playerX.getPlayerSign());
+        if (gameType === 'one') {
+          Npc.makeMove();
+          round += 1;
+        }
+      } else if (round % 2 === 0) {
+        if (gameType === 'two') {
+          addMark(place, playerO.getPlayerSign());
+        }
+      }
       endRound('x');
       endRound('o');
     }
   };
-  return { startRound, resetRound };
+  return {
+    startRound, resetRound, addMark, round, gameTypee,
+  };
 })();
